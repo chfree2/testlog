c0934880678f8a211c583cbcad697469a654a91d
huangxg <huangxg@ctitech.cn>
2016-08-22 16:24:48 +0800

    test

---
+++ testWebLogin/Controllers/textController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace testWebLogin.Controllers
+{
+    public class textController : Controller
+    {
+        //
+        // GET: /text/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ testWebLogin/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="testWebLogin.MvcApplication" Language="C#" %>
